3ea6d07d26c3eb653e35fdbc71a5e5ba3eff0365
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-04-05 14:12:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var dev_db_url = "mongodb+srv://albert:V9ur3MadthzqgFR7@cluster0-xa9pk.mongodb.net/local-library?retryWrites=true&w=majority"
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
